9f0a40ee35b80035d95a594c0347af3138554dea
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2025-12-10 23:56:24 +0100

    feat: day 10 part 2 - wip

--- src/day10.py
+++ src/day10.py
@@ -1,13 +1,21 @@
-from dataclasses import dataclass
+from collections import Counter
+from dataclasses import dataclass, field
 import itertools
 import re
+import time
 
 
 @dataclass
 class Machine:
     diagram: list[bool]
-    buttons: list[list[int]]
-    joltage_req: list[int]
+    buttons: list[tuple[int, ...]]
+    joltage_target: list[int]
+    n_lights: int = field(init=False)
+    n_buttons: int = field(init=False)
+
+    def __post_init__(self):
+        self.n_lights = len(self.diagram)
+        self.n_buttons = len(self.buttons)
 
     @classmethod
     def from_manual(cls, manual_line: str):
@@ -20,20 +28,62 @@ class Machine:
         diagram = [chr == "#" for chr in diagram_str[0].strip("[]")]
         joltage = [int(j) for j in joltage_str[0].strip("{}").split(",")]
         buttons = [
-            [int(n) for n in b[0].strip("()").split(",")]  # pyright: ignore [reportAny]
+            tuple([int(n) for n in b[0].strip("()").split(",")])  # pyright: ignore [reportAny]
             for b in re.findall(r"(\(\d+(,\d+)*\))", manual_line)  # pyright: ignore [reportAny]
         ]
 
         return cls(diagram, buttons, joltage)
 
     def find_fewest_presses(self) -> int:
-        for n in range(1, len(self.buttons) + 1):
+        for n in range(1, self.n_buttons + 1):
             for buttons in itertools.combinations(self.buttons, n):
-                lights = [False for _ in self.diagram]
+                lights = [False] * self.n_lights
                 for b in buttons:
                     for light in b:
                         lights[light] = not lights[light]
                 if lights == self.diagram:
+                    return n
+
+        return -1
+
+    def find_fewest_presses_p2(self, i: int, n: int) -> int:
+        start_time = time.time()
+        print(
+            f"Checking machine {i}/{n} with {self.n_lights} lights and {self.n_buttons} buttons"
+        )
+        result = self.find_fewest_presses_p2_aux()
+        end_time = time.time()
+        print(f"-> Time: {end_time - start_time:.4f} seconds")
+        return result
+
+    def find_fewest_presses_p2_aux(self) -> int:
+        def check_button_sequence(button_sequence: Counter[tuple[int, ...]]):
+            counter = [0] * self.n_lights
+            for b, count in button_sequence.items():
+                for light in b:
+                    counter[light] += count
+                    if counter[light] > self.joltage_target[light]:
+                        return False
+            return counter == self.joltage_target
+
+        target_sum = sum(self.joltage_target)
+        max_size = max(map(lambda b: len(b), self.buttons))
+
+        n = 0
+        while n < 100:
+            n += 1
+            if n * max_size < target_sum:
+                continue
+            print(f"-> Trying {n} buttons")
+            for button_sequence in itertools.combinations_with_replacement(
+                self.buttons, n
+            ):
+                increase = sum(map(lambda b: len(b), button_sequence))
+                if increase != target_sum:
+                    continue
+
+                button_sequence_with_count = Counter(button_sequence)
+                if check_button_sequence(button_sequence_with_count):
                     return n
 
         return -1
@@ -55,3 +105,9 @@ with open("inputs/day10.txt", encoding="utf-8") as file:
 machines = parse_input(actual)
 p1 = sum(m.find_fewest_presses() for m in machines)
 print(f"Part 1: {p1}")
+
+p2 = sum(
+    m.find_fewest_presses_p2(i + 1, len(machines))
+    for i, m in enumerate(machines)
+)
+print(f"Part 1: {p2}")
